86e52355fca2e76b1f16872ca4f2a42428b9e565
Noor <noor.alamvfirst@gmail.com>
2022-05-12 15:22:41 +0530

    3rd commit after tailwind configure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import {BsFillBarChartFill} from "react-icons/bs"
 
+
 function App() {
   return (
-    <div className="App">
-      <h1><BsFillBarChartFill/>Your appointments</h1>
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl">
+        <BsFillBarChartFill className="inline-block text-red-400 align-auto"/>Your appointments</h1>
     </div>
   );
 }
