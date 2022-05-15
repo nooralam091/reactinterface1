fdaa8ff447c02d9e865729e27c7ecc8cc8cf7fd2
Noor <noor.alamvfirst@gmail.com>
2022-05-15 09:35:24 +0530

    After appointment component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import {BsFillBarChartFill} from "react-icons/bs"
-
+import { BsFillBarChartFill } from "react-icons/bs";
+import AddAppointment from "./components/AddApointments";
+import Search from "./components/Search";
+import AppointmentInfo from "./components/AppointmentInfo";
+import AppontmentList from "./data.json"
 
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
-      <h1 className="text-5xl">
-        <BsFillBarChartFill className="inline-block text-red-400 align-auto"/>Your appointments</h1>
+      <h1 className="text-5xl mb-3">
+        <BsFillBarChartFill className="inline-block text-red-400 align-auto" />
+        Your appointments
+      </h1>
+      <AddAppointment />
+      <Search />
+
+      <ul className="divide-y divide-gray-200">
+        {AppontmentList.map((appointment) => (
+          <AppointmentInfo key={appointment.id} appointment={appointment}/>
+        ))}
+      </ul>
     </div>
   );
 }
